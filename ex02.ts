15ae11db2fa9d2ae8a58b0584ba556eb0bea7a71
Andrew ABDELMALAK <andrew.abdelmalak@efrei.net>
2021-10-04 12:51:05 +0200

    Adding.author.json

---
+++ ex02.ts
@@ -0,0 +1,19 @@
+function upperCase(str = '') {
+    let result = ''
+
+    for (let i=0; i < str.length; i++) {
+      const code = str.charCodeAt(i)
+
+      if (code >= 97 && code <= 122) {
+        result += String.fromCharCode(code - 32)
+      } else {
+        result += str[i] 
+      }
+    }
+
+    return result
+  }
+
+  console.log(upperCase('Chopper'))
+  console.log(upperCase('tony tony'))
+  
